f79bdc9ce4c6927797d11adc638cd372cd23b325
Maicon <maiconmoreirams@hotmail.com>
2019-10-18 20:24:21 -0400

    Renderizando a tabela

--- aula03/src/js/index.js
+++ aula03/src/js/index.js
@@ -1,30 +1,151 @@
 /**
- * Nosso BD fake!"
- * (in memory DB).
+ * Nosso BD fake!
+ * (in memory DB)
  */
+var contatos = [
+   {
+      nome:'Maicon',
+      telefone: '99 9 99999999',
+      email:'maiconmoreirams@hotmail.com',
+      dataNascimento:'26/09/1988'
+   }
+];
+renderizarTabelaContatos();
 
- var contatos = [];
- 
-
- function salvarContato(event) {
-     //inibi a recara da pagina
+function salvarContato(event) {
+    // Inibe a recarga da página
     event.preventDefault();
 
-    //recupera os valores do formulario
-    //$ -> document.querySelector
-    //val -> value
+    /**
+     * $ -> document.querySelector
+     * val() -> value
+     */
+    // Recupera os valores do formulário
+    let nome = $('#nome').val();
+    let telefone = $('#telefone').val();
+    let email = $('#email').val();
+    let dataNascimento = $('#dataNascimento').val();
 
-     let nome = $('#nome').val();
-     let telefone = $('#telefone').val();
-     let email = $('#email').val();
-     let dataNascimento = $('#dataNascimento').val();
+    // Criar um objeto de contato
+    let contato = {
+        nome,
+        telefone,
+        email,
+        dataNascimento
+    };
 
-      //criar umk objeto de contato
- let contato = {nome,telefone,email,dataNascimento};
+    // Adiciona o contato no nosso BD (no final do vetor)
+    contatos.push(contato);
 
- //adiciona um contato no nosso BD(no final do vetor)
- contatos.push(contato);
+    // Invoca a renderização da tabela
+    renderizarTabelaContatos();
+}
 
- console.log(contatos);
-     
- }
+function renderizarTabelaContatos() {
+    if (contatos.length > 0) {
+        let areaListagemContatos =
+            document.getElementById('listagemContatos');
+
+        /**
+         * Limpa a área de listagem
+         */
+        areaListagemContatos.innerHTML = '';
+
+        /*
+        areaListagemContatos.innerHTML = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Telefone</th>
+                        <th>E-mail</th>
+                        <th>Data Nasc.</th>
+                    </tr>
+                </thead>
+            </table>
+        `;
+        */
+
+        /**
+         * Cria a tabela
+         */
+        let tabela = document.createElement('table');
+
+        let cabecalho = criarCabecalhoTabela();
+        //adiciona o corpo da cabecalho na tabela
+        tabela.appendChild(cabecalho);
+
+        let corpoTabela = criarCorpoTabela();
+        //adicionar o corpo da tabela na tabela
+        tabela.appendChild(corpoTabela); 
+
+        // Adiciona o corpo da tabela na tabela
+        tabela.appendChild(corpoTabela);
+
+        // Adiciona a tabela na área de listagem
+        areaListagemContatos.appendChild(tabela);
+
+
+    }
+}
+
+function criarCabecalhoTabela() {
+    /**
+     * Cria o cabeçalho da tabela
+     */
+    let cabecalho = document.createElement('thead');
+    let linhaCabecalho = document.createElement('tr');
+    let colunaNome = document.createElement('th');
+    colunaNome.innerText = 'Nome';
+    let colunaTelefone = document.createElement('th');
+    colunaTelefone.innerText = 'Telefone';
+    let colunaEmail = document.createElement('th');
+    colunaEmail.innerText = 'E-mail';
+    let colunaDataNasc = document.createElement('th');
+    colunaDataNasc.innerText = 'Data Nasc.';
+
+    // Adiciona as colunas na linha do cabeçalho
+    linhaCabecalho.appendChild(colunaNome);
+    linhaCabecalho.appendChild(colunaTelefone);
+    linhaCabecalho.appendChild(colunaEmail);
+    linhaCabecalho.appendChild(colunaDataNasc);
+
+    // Adiciona a linha do cabeçalho no cabeçalho
+    cabecalho.appendChild(linhaCabecalho);
+
+    // Retorna o cabeçalho criado
+    return cabecalho;
+}
+
+function criarCorpoTabela() {
+    /**
+     * Cria o corpo da tabela
+     */
+    let corpoTabela = document.createElement('tbody');
+
+    /**
+     *  Cria as linhas de contatos
+     */
+    for (let i = 0; i < contatos.length; i++) {
+        /**
+         * Cria uma nova linha no corpo da tabela
+         */
+        let linha = document.createElement('tr');
+        let celulaNome = document.createElement('td');
+        celulaNome.innerText = contatos[i].nome;
+        linha.appendChild(celulaNome);
+        let celulaTelefone = document.createElement('td');
+        celulaTelefone.innerText = contatos[i].telefone;
+        linha.appendChild(celulaTelefone);
+        let celulaEmail = document.createElement('td');
+        celulaEmail.innerText = contatos[i].email;
+        linha.appendChild(celulaEmail);
+        let celulaDataNasc = document.createElement('td');
+        celulaDataNasc.innerText = contatos[i].dataNascimento;
+        linha.appendChild(celulaDataNasc);
+
+        // Adiciona a nova linha no corpo da tabela
+        corpoTabela.appendChild(linha);
+    }
+    return corpoTabela; 
+}
--- aula03/src/js/mascaras.js
+++ aula03/src/js/mascaras.js
@@ -1,8 +1,9 @@
 /**
- * Utilizaremos a variavel global do jquery ($) para carregar
- * a mascara de telefone assim que a pagina for carregada
+ * Utilizaremos a variavel global do jquery (S) para carregar a mascara de telefone assim que a pagina for carregada.
  */
-$(document).ready(function(){
-    //implementa a mascara de telefone
+
+$(document).ready(function (){
+    // Implementa a Mascara de Telefone
     $('#telefone').mask('(00) 00000-0000');
-})
+
+ });
